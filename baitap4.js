9cc1cc95349ce573f89d112957932f1936177150
MyTPH <tan.phamhongmy@gmail.com>
2025-03-13 15:42:35 +0900

    baitap4.js

---
+++ baitap4.js
@@ -0,0 +1,9 @@
+let width = 20;
+
+let height = 10;
+
+let area = width * height;
+
+document.write("width = " + width + "<br/>");
+document.write("height = " + height + "<br/>");
+document.write("area = " + area);
