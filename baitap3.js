169abedef73d0ba8b4e4b69cd6956ca2a38da68b
MyTPH <tan.phamhongmy@gmail.com>
2025-03-13 15:35:00 +0900

    baitap3.js

---
+++ baitap3.js
@@ -0,0 +1,15 @@
+console.log("welcome to Rikkei Academy");
+
+document.write("Quyết tâm học lập trình javascript");
+
+alert("Chào mừng bạn đến với học viện Rikkei Academy");
+
+let isAdult = confirm("Bạn đã đủ 18 tuổi chưa?");
+
+document.write("<br/>")
+
+if (isAdult) {
+    document.write("Bạn đã đủ 18 tuổi.");
+} else {
+    document.write("Bạn chưa đủ 18 tuổi.");
+}
