978c4267515db942f190c82d2f831aba790c9b54
MyTPH <tan.phamhongmy@gmail.com>
2025-03-16 01:11:42 +0900

    baitap7.js

---
+++ baitap7.js
@@ -0,0 +1,5 @@
+let math = +prompt("Mời bạn nhập điểm môn Toán: ");
+let physics = +prompt("Mời bạn nhập điểm môn Vật lý: ");
+let chemistry = +prompt("Mời bạn nhập điểm môn Hóa học: ");
+let average = (math + physics + chemistry) / 3;
+document.write("Điểm trung bình của bạn là: " + average);
